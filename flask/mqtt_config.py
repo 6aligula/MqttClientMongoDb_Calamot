c285585330ad2e9c2f5f7e2f82fc14c361300fb9
6aligula <vinicio.naranjo15@gmail.com>
2024-05-07 18:49:10 +0200

    add exceptions in conexion mqtt_config

--- flask/mqtt_config.py
+++ flask/mqtt_config.py
@@ -4,13 +4,18 @@ from settings import Config
 
 def create_mqtt_client(on_connect, on_message):
     client = mqtt.Client()
-    if Config.MQTT_USERNAME and Config.MQTT_PASSWORD:
-        client.username_pw_set(Config.MQTT_USERNAME, Config.MQTT_PASSWORD)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(Config.MQTT_BROKER, 1883, 60)
-    client.loop_start()
+    try:
+        if Config.MQTT_USERNAME and Config.MQTT_PASSWORD:
+            client.username_pw_set(Config.MQTT_USERNAME, Config.MQTT_PASSWORD)
+
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect(Config.MQTT_BROKER, 1883, 60)
+        client.loop_start()
+    except Exception as e:
+        print(f"Error connecting to MQTT broker: {e}")
     return client
+
 
 def publish_message(client, topic, message):
     try:
